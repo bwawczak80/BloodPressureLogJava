3acce5d9650cd4a6516faabfb110428f7d783549
bwawczak80 <35830914+bwawczak80@users.noreply.github.com>
2019-04-13 00:07:13 -0500

    updated main screen layout

--- app/src/main/java/net/wawczak/brian/bloodpressurelog/LogBpFragment.java
+++ app/src/main/java/net/wawczak/brian/bloodpressurelog/LogBpFragment.java
@@ -7,12 +7,67 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class LogBpFragment extends Fragment {
+
+    double sys;
+    double dia;
+    double bpm;
+    String note;
+    String time;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_log_bp, container, false);
+
+
+        View v = inflater.inflate(R.layout.fragment_log_bp, container, false);
+        final EditText systolic = v.findViewById(R.id.idSystolicInput);
+        final EditText diastolic = v.findViewById(R.id.idDiastolicInput);
+        final EditText pulse = v.findViewById(R.id.idPulseInput);
+        final EditText notes = v.findViewById(R.id.idNotesInput);
+        TextView timeDisplay = v.findViewById(R.id.idTimeDisplay);
+        TextView logDisplay = v.findViewById(R.id.idLogDisplay);
+        Button logBp = v.findViewById(R.id.btnLogBP);
+
+        timeDisplay.setText(dateTimeStamp());
+
+        logBp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                time = dateTimeStamp();
+                String holdSys = systolic.getText().toString();
+                String holdDia = diastolic.getText().toString();
+                String holdPulse = pulse.getText().toString();
+                note = notes.getText().toString();
+
+                try {
+                    sys = Double.parseDouble(holdSys);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+
+
+        return v;
     }
+
+
+    public String dateTimeStamp() {
+        Date myDate = new Date();
+        return DateFormat.getDateTimeInstance().format(myDate);
+    }
+
 }
