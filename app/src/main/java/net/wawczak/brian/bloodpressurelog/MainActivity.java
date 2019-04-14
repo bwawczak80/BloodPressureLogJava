c7fcb881da91c28c003a1ea52de09cd54ab4f4fe
bwawczak80 <35830914+bwawczak80@users.noreply.github.com>
2019-04-14 04:00:33 -0500

    fixed warnings

--- app/src/main/java/net/wawczak/brian/bloodpressurelog/MainActivity.java
+++ app/src/main/java/net/wawczak/brian/bloodpressurelog/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
